47eef8f8e5ecda211427402e5ba8e4af408f6407
loukaspd <loukaspd@gmail.com>
2023-03-17 10:27:01 +0200

    [Solution] include external libraries inside the app

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,5 @@
-<svelte:head>
-	<!-- Material Icons 
-	choose icons from here:https://fonts.google.com/icons
-	-->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-	<!-- jquery -->
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js" on:load={onJqueryLoaded}></script>
-	<!-- Fomantic ui-->
-	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.1/dist/semantic.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.1/dist/semantic.min.js"></script>
-</svelte:head>
-
 <script>
 	import Main from './Main.svelte';
-
-	let jQueryLoaded = false;
-
-	function onJqueryLoaded() {
-		console.log('jQuery loaded');
-		jQueryLoaded = true;
-	}
 </script>
 
-{#if jQueryLoaded}
-	<Main />
-{:else}
-	<h1> loading...</h1>
-{/if}
+<Main />
